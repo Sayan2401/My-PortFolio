0da5f3da545f963b14fde9c3f588b030a9399d20
Sayan2401 <sayanparmar36@gmail.com>
2025-06-22 14:19:59 +0530

    unused removed

--- src/Components/Rightside.jsx
+++ src/Components/Rightside.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Header from './Header';
 import Hero from './Hero';
 import About from './About';
-import Services from './Services';
+// import Services from './Services';
 
 const Rightside = () => {
     return (
